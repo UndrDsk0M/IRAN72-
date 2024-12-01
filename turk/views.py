92083ed539277aece8dc314e377ca37103fc778b
UndrDsk0M <onlymail0001@gmail.com>
2024-12-01 18:49:25 +0200

    adding search part

--- turk/views.py
+++ turk/views.py
@@ -6,6 +6,5 @@ from django.shortcuts import get_object_or_404
 def finding_word(request):
     quary = request.GET.get('q', '')
     word = get_object_or_404(word_tr, word_pr=quary )
-    print(quary)
-    return HttpResponse(word)
+    return render(request, 'searchpage.html', {'word': word})
 
